2e1685e603038e4605a6a071899c396ff4b5b962
nathantum <tumkipsang@gmail.com>
2023-07-03 23:28:37 -0500

    added more files

--- Dart_Programming/04-Exercise-for-loops.dart
+++ Dart_Programming/04-Exercise-for-loops.dart
@@ -1,22 +1,29 @@
-void main(){
-  
-  var myList = [1,2,3,4,5,6,7,8,9,10];
-  
+void main() {
+  var myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   //TASK ONE:
   // Use a for loop with iterable iteration
   // to print out the numbers in myList
-   
+  for (final num in myList) {
+    print(num);
+  }
+
   //TASK TWO:
   // Use a for loop with incrementation i++
   // to print out the numbers in myList
-   
-  
+  for (var i = 0; i < myList.length; i++) {
+    print(myList[i]);
+  }
+
   //TASK THREE:
   // Use any for loop type to print out
   // only the EVEN numbers in this list
   // HINT: Modulo Operator in Dart is %
   // https://dart.dev/language/operators#arithmetic-operators
-  // You will need if statement to check 
-   
-  
+  // You will need if statement to check
+  for (final num in myList) {
+    if (num % 2 == 0) {
+      print(num);
+    }
+  }
 }
--- Dart_Programming/07-Exercise-While-Loops.dart
+++ Dart_Programming/07-Exercise-While-Loops.dart
@@ -1,2 +1,11 @@
-// Write a Dart program that prints the 
+// Write a Dart program that prints the
 //  first 10 positive even numbers using a while loop.
+void main(List<String> args) {
+  var count = 1;
+  var num = 0;
+  while (count <= 10) {
+    num += 2;
+    print(num);
+    count++;
+  }
+}
